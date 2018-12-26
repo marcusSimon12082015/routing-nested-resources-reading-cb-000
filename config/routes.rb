b95135f92178415444dd0b5ea9aa6cd3aa5a4510
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-26 16:04:02 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
       # nested resource for posts
       resources :posts, only: [:show, :index]
     end
-   
+
     resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-   
+
     root 'posts#index'
 end
